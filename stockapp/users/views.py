b9d5bcff0940b3b4a7aa9fe3dd07631e4ee61201
Piyush Raj <piyushraj.270203@gmail.com>
2024-12-30 22:25:05 +0530

    added pylint

--- stockapp/users/views.py
+++ stockapp/users/views.py
@@ -1,8 +1,17 @@
+"""
+This module contains the views for the users app.
+It handles user-related actions such as login, logout, and profile management.
+"""
+
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    """To handle the home page view"""
+
     return render(request, 'users/home.html')
 
 def signup(request):
+    """To handle the signup page view. Never used in the project."""
+
     return render(request, 'users/signup.html')
